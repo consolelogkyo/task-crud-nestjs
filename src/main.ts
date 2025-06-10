6bfb2cce1193c0d2a20904b52209774f61966d59
matheusolovics@gmail.com <kyo@DESKTOP-SNTM95N.>
2025-06-09 21:08:41 -0300

    feat: class validator and partialType

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 // - 'src/app/app.module.ts': É o modulo principal da aplicação NestJS, onde são importados outros módulos, controladores e provedores.
 // - 'src/app/app.controller.ts': Define o controlador principal da aplicação, que lida com as rotas e requisições HTTP.
@@ -8,6 +9,12 @@ import { AppModule } from './app/app.module';
 // Arquivo que inicia a aplicação NestJS
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,5 +1,9 @@
-export class UpdateTaskDTO {
-  readonly name?: string;
-  readonly description?: string;
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateTaskDTO } from './create-task.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
+
+export class UpdateTaskDTO extends PartialType(CreateTaskDTO) {
+  @IsBoolean()
+  @IsOptional()
   readonly completed?: boolean;
 }
